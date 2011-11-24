2ffa146b8a46b51dff46023d68a52e90d8949b41
vinoj.cardoza <vinoj.cardoza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-24 18:31:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cardoza-3d-tag-cloud/trunk@467160 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cardoza_3D_tag_cloud.php
@@ -0,0 +1,48 @@
+<?php
+   /*
+   Plugin Name: Cardoza 3D tag cloud
+   Plugin URI: http://fingerfish.com/cardoza-3d-tagcloud/
+   Description: Cardoza 3D tag cloud displays your tags in 3D by placing them on a rotating text.
+   Version: 1.0
+   Author: Vinoj Cardoza
+   Author URI: http://fingerfish.com/about-me/
+   License: GPL2
+   */
+
+//includes the jquery file
+wp_enqueue_script('tagcloud_handle', plugin_dir_url(__FILE__). 'cardoza_3D_tag_cloud.js', array('jquery'));
+//includes the css styles file
+wp_enqueue_style('my-style', plugin_dir_url(__FILE__). '3dcloud_style.css');
+
+add_action("plugins_loaded", "cardoza_3d_tagcloud_init");
+
+function widget_cardoza_3d_tagcloud($args){
+	extract($args);
+	echo $before_widget;
+	echo $before_title;
+	echo "Tag Cloud";
+	echo $after_title;
+	global $wpdb;
+	$tags_list = get_terms('post_tag', array(
+				'orderby' 		=> 'count',
+				'hide_empty' 	=> 0
+			));
+	if(sizeof($tags_list)!=0){
+		$max_count = 0;
+		foreach($tags_list as $tag) if($tag->count > $max_count) $max_count = $tag->count;
+		echo '<div id="list"><ul>';
+		foreach($tags_list as $tag){
+			$font_size = 40 - (($max_count - $tag->count)*2);
+			if($font_size<12) $font_size = 12;
+			echo '<li><a href="'.$_SERVER['PHP_SELF'].'?tag='.$tag->slug.'" style="font-size:'.$font_size.'px;">'.$tag->name.'</li>';
+		}
+		echo '</ul></div>';
+	}
+	else echo "No tags found";
+	echo $after_widget;
+}
+
+function cardoza_3d_tagcloud_init(){
+	register_sidebar_widget(__('Cardoza\'s 3D Tag Cloud'), 'widget_cardoza_3d_tagcloud');
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Cardoza 3D Tag Cloud ===
+Contributors: Vinoj Cardoza
+Tags: tag, tagcloud, cloud, 3D tagcloud
+Requires at least: 3.0
+Tested up to: 3.2
+Stable tag: trunk
+License: GPLv2 or later
+
+Displays your post tags in 3D rotating animation.
+
+== Description ==
+
+Cardoza 3D Tag Cloud allows you to display your site’s tags using jquery that rotates the tags in 3D. 
+
+It works just like a regular tags cloud, but is more visually exciting. Clicking the tags will take you to the appropriate page. 
+
+For more visit: http://fingerfish.com/cardoza-3d-tagcloud/
+
+== Installation ==
+
+1. Download the plugin.
+2. Upload to your blog (/wp-content/plugins/).
+3. Activate it.
+4. Then go to widget and drag and drop Cardoza's 3d Tag Cloud in the area you want to display the plugin.
+
+You're done!
+
+Uninstalling is as simple as deactivating the plugin.
+
+== Screenshots ==
+
+1. screenshot-1.gif
+
+== Frequently Asked Questions ==
+
+None yet, if you have one post it at http://fingerfish.com/cardoza-3d-tagcloud/
+
