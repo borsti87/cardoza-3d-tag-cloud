cb2cd7a3a5a86f6561023fcaa151ad5fc8d580dc
vinoj.cardoza <vinoj.cardoza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-03 14:11:07 +0000

    1.9 tag updated
    git-svn-id: https://plugins.svn.wordpress.org/cardoza-3d-tag-cloud/tags/1.9@647457 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cardoza_3D_tag_cloud.js
@@ -0,0 +1,16 @@
+$j = jQuery.noConflict();
+$j(document).ready(function() {
+    if(!$j('#myCanvas').tagcanvas({
+        textColour: '#333333',
+        outlineColour: '#FFFFFF',
+        reverse: true,
+        depth: 0.8,
+        textFont: null,
+        weight: true,
+        maxSpeed: 0.05
+    },'tags')) {
+        // something went wrong, hide the canvas container
+        $j('#myCanvasContainer').hide();
+    }
+});
+
